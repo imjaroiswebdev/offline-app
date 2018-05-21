3aeb0839706656146af390d38506e9621061cf79
José Antonio Reyes <imjaroiswebdev@gmail.com>
2018-05-21 14:20:18 -0400

    Updates deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-echo "travis_fold:end:deploy" && [ "${TRAVIS_PULL_REQUEST}" = "false" ] && ["${TRAVIS_BRANCH}" = "master" ] && echo "Deploying…" && gulp deploy
+echo "travis_fold:end:deploy"
+[ "${TRAVIS_PULL_REQUEST}" = "false" ]
+["${TRAVIS_BRANCH}" = "master" ]
+echo "Deploying…"
+gulp deploy
