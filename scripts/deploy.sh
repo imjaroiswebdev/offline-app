51e9ca16014f97ee7dd91011cc92bf135cf88b5b
José Antonio Reyes <imjaroiswebdev@gmail.com>
2018-05-21 18:57:12 -0400

    Updates deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-echo "travis_fold:end:deploy"
-export TRAVIS_PULL_REQUEST="false"
-export TRAVIS_BRANCH="master"
-echo "Deploying…"
+echo "travis_fold:end:deploy" && \
+export TRAVIS_PULL_REQUEST="false" && \
+export TRAVIS_BRANCH="master" && \
+echo "Deploying…" && \
 npm install && gulp deploy
